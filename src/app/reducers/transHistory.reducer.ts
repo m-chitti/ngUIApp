01d9077393fc6681f9d09753004c344a5c1fabbe
m-chitti <chittimadhu1@gmail.com>
2018-12-25 22:01:59 -0500

    Update transHistory.reducer.ts

--- src/app/reducers/transHistory.reducer.ts
+++ src/app/reducers/transHistory.reducer.ts
@@ -1,18 +1,9 @@
 import { TransactionHistory } from '../transHistory/transaction-history';
-import { Action, MetaReducer, ActionReducer } from '@ngrx/store';
-import { localStorageSync, rehydrateApplicationState } from 'ngrx-store-localstorage';
-import {AppState} from '../app.state'
-import { environment } from '../../environments/environment';
-import { logging } from 'protractor';
+import { Action } from '@ngrx/store';
 
 export const ADD_TRANS_HIST = 'ADD_TRANS_HIST';
 
-export class Storage implements Action {
-    readonly type = '@ngrx/store/storage';
-    constructor(readonly payload: string) { }
-  }
-
-export function addTransHistReducer(state: TransactionHistory[]=[], action) {
+export function addTransHistReducer(state: TransactionHistory[] = [], action) {
   switch (action.type) {
     case ADD_TRANS_HIST:
         return [...state, action.payload];
@@ -20,28 +11,3 @@ export function addTransHistReducer(state: TransactionHistory[]=[], action) {
         return state;
     }
 }
-
-   
-  export function localStorageSyncReducer(addTransHistReducer: ActionReducer<AppState>): ActionReducer<AppState> {
-     
-    return (state: AppState, action: any) => {
-        console.log("==============  "+action.type)
-      const keys = ['session'];
-      debugger;
-    //   if ((action.type === ADD_TRANS_HIST )) {
-    //     return [state, action.payload];
-    //   }
-      if ((action.type === '@ngrx/store/init')) {
-        const rehydratedState = rehydrateApplicationState([action.payload], localStorage, k => k, false);
-        return { ...state, ...rehydratedState };
-      }
-      return localStorageSync({ keys, rehydrate: true, })(addTransHistReducer)(state, action);
-    }
-  }
-  
-  export const metaReducers: MetaReducer<AppState>[] = ! environment.production
-    ? [localStorageSyncReducer]
-    : [localStorageSyncReducer];
-
-
-
